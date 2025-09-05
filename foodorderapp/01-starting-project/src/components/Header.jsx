c8fa4ed2926286c4deba2bac24e0a5c9f49c8175
arunsla <arunraj.softlogic@gmail.com>
2025-09-05 09:54:46 +0530

    update

---
+++ foodorderapp/01-starting-project/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import logoImg from '../assets/logo.jpg'
+
+function Header() {
+  return (
+    <header id='main-header'>
+        <div id='title'>
+            <img src={logoImg} alt="A restaurant" />
+            <h1>React Food APP </h1>
+        </div>
+        <nav><button>Cart (0)</button></nav>
+    </header>
+  )
+}
+
+export default Header
